2535cc29c575ef7be69f79d78909c5c101cdaa2f
yudiptr <94793172+yudiptr@users.noreply.github.com>
2022-10-15 22:07:27 +0700

    add menu login

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from example_app.views import index
+from example_app.views import login
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login, name='login'),
 ]
--- example_app/views.py
+++ example_app/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("todolist:show_todo")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
