9fdf5fa4be4fd4638e275c5643175dbf994cda22
dianisawulandari <dianisawulandari@gmail.com>
2022-10-21 22:11:56 +0700

    views operasi

--- operasi/urls.py
+++ operasi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from operasi.views import show_jadwal_operasi
+from operasi.views import show_jadwal_operasi, jadwal_operasi_json, add_jadwal_operasi
 
 app_name = 'operasi'
 
 urlpatterns = [
     path('', show_jadwal_operasi, name='show_jadwal_operasi'),
+    path('json/', jadwal_operasi_json, name='jadwal_operasi_json'),
+    path('add/', add_jadwal_operasi, name='add_jadwal_operasi')
 ]
--- operasi/views.py
+++ operasi/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
 from operasi.models import Operasi
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
 
 # Create your views here.
 def show_jadwal_operasi(request):
     user = request.user
-    # if user.isDoctor():
-    #     data = Operasi.objects.filter(dokter = user)
-    # else:
-    #     data = Operasi.objects.filter(pasien = user)
-    data = Operasi.objects.filter(pasien = user) # nanti ini dihapus
     context = {
     'user': user.username,
-    'data': data
     }
     return render(request, 'jadwaloperasi.html', context)
+
+def add_jadwal_operasi(request):
+    if request.method == 'POST':
+        pasien = request.POST.get('get_pasien')
+        jenis = request.POST.get('get_jenis')
+        tanggal = request.POST.get('get_tanggal')
+        jam = request.POST.get('get_jam')
+        durasi = request.POST.get('get_durasi')
+        keterangan = request.POST.get('get_keterangan')
+
+        operasi = Operasi(dokter = request.user, pasien = pasien,\
+                    jenis = jenis, tanggal = tanggal, jam = jam,\
+                    durasi = durasi, keterangan = keterangan)
+
+        operasi.save()
+    return render(request, 'jadwaloperasi.html')
+
+def jadwal_operasi_json(request):
+    user = request.user
+    if user.related_name == 'dokter':
+        data = Operasi.objects.filter(dokter = user)
+    else:
+        data = Operasi.objects.filter(pasien = user)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
